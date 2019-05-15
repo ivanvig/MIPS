98a44cdfdb9d1436088bc9e83eaa727ee306fcb9
Ivan Vignolles <ivanmvig@gmail.com>
2019-05-15 16:40:24 -0300

    sacando nops

--- src/fib.asm
+++ src/fib.asm
@@ -7,23 +7,10 @@
 
   OR    $t4, $0, $0
   OR    $t3, $0, $0
-
-  SLL   $0, $0, 0
-  SLL   $0, $0, 0
-  SLL   $0, $0, 0
-
 LOOP:
   ADDU  $t1, $t2, $t4
-  SLL   $0, $0, 0
-  SLL   $0, $0, 0
-  SLL   $0, $0, 0
-  SLL   $0, $0, 0
   OR    $t4, $0, $t2
   OR    $t2, $0, $t1
   SW    $t1, 0($t3)
   ADDI  $t3, $t3, 1
-  SLL   $0, $0, 0
-  SLL   $0, $0, 0
-  SLL   $0, $0, 0
-  SLL   $0, $0, 0
   BNE   $t0, $t3, LOOP
